d865da542bf2d3328aa2ae7201aec9069163d83e
Adenike Adeyemi <nike.adeyemi1@gmail.com>
2022-02-23 23:16:08 -0500

    I can get and filter key inputs that are not letters

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -1,4 +1,4 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import {Status} from './Status'
 import { PlayArea } from './PlayArea'
 
@@ -14,7 +14,7 @@ function initBoard(Dictionary){
         word.display.push((word.answer.charAt(i)===' ') ? '\u00A0':'_')
     }
 
-    console.log(word);
+    // console.log(word);
     
     return word;
 }
@@ -24,7 +24,7 @@ export function GameBoard(){
     let Dictionary = ["Hello My Friend"]
     initBoard(Dictionary);
     
-    let newGame = true;
+    const [newGame, setNewGame] = useState(true);
     //Status data
     const [gameStatus, setGameStatus] = useState({
         wins: 0,
@@ -49,7 +49,23 @@ export function GameBoard(){
          setGameStatus(previousState => {
              return {...previousState, guesses:10, letters:[]}
          })
-         newGame=false;
+         setNewGame(false);
+        }
+        else{
+            //Filter out all keys except uppercase and capital letters
+            if(e.key.length > 1 || (e.key.charCodeAt(0) < 65) || ((e.key.charCodeAt(0) > 90) && (e.key.charCodeAt(0) < 97 )) || (e.key.charCodeAt(0) > 122)) return;
+
+            console.log(e.key.charCodeAt(0));
+            //TODO compare keyed in letters with answer string.
+            //if match is found update display
+            //else check if letter had been guessed
+            //if not guessed yes push into guessed list decrement number of guesses
+            //else notify user that letter has been used already
+            //Check if we won AKA are there any "_" left and guesses left is not 0
+            //if there are no "_" and guess left is not 0 mincrement win ask user to play again
+            //if guess are 0 you lose, state the answer, ask user to play again.
+            //initialize and start new game.
+            
         }
     }
      
